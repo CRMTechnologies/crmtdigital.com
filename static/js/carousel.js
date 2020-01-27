79a9782c0c3607007714b42efa493d850a57be34
Alan Chatfield <47448740+AlanChatfield@users.noreply.github.com>
2020-01-27 22:28:58 +0000

    Create carousel.js

---
+++ static/js/carousel.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){		
+  $('.logo-carousel').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});  
