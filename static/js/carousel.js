ef6556ab5a8f0ed54502060e021aa17a1bda84f2
Alan Chatfield <47448740+AlanChatfield@users.noreply.github.com>
2020-01-27 22:40:31 +0000

    Update carousel.js

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -5,25 +5,18 @@ $(document).ready(function(){
     autoplay: true,
     arrows: false,
     speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     responsive: [
       {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
-        }
-      },
-      {
-        breakpoint: 600,
+        breakpoint: 992,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 576,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
