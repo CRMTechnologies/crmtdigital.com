571828fd49c6e557b0ae4bdd7ba6ce92d50c49b4
Alan Chatfield <47448740+AlanChatfield@users.noreply.github.com>
2020-08-28 22:28:11 +0100

    Update submission-created.js

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,12 +1,10 @@
 const querystring = require('querystring');
 var https = require('https');
 
-exports.handler = function (event, context) {
+exports.handler = function (event, context, callback) {
 	var post_data = querystring.stringify(
 		event.body
 	);
-	var post_status = 200;
-	var post_response = "Done";
 
   // An object of options to indicate where to post to
   var post_options = {
@@ -24,12 +22,16 @@ exports.handler = function (event, context) {
   var post_req = https.request(post_options, function(res) {
       res.setEncoding('utf8');
       res.on('data', function (chunk) {
-		post_status = 200;
-		console.log( "Done" );
+		callback(null, {
+			statusCode: 200,
+			body:  "Done" 
+		});	
       });
       res.on('error', function (e) {
-		post_status = 400;
-		console.log( "Failed: " + e.message ); 	  
+		callback(null, {
+			statusCode: 400,
+			body:  "Failed " + e.message 
+		});	 	  
       });
 
   });
@@ -37,4 +39,5 @@ exports.handler = function (event, context) {
   // post the data
   post_req.write(post_data);
   post_req.end();
+
 }
