05fd1b59c2fa1d476b493b1d0c6cb1bb8fc3311d
Alan Chatfield <47448740+AlanChatfield@users.noreply.github.com>
2020-08-28 22:09:39 +0100

    Update submission-created.js

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,17 +1,43 @@
-const request = require('request');
+var https = require('https');
 
-exports.handler = async function (event, context, callback) {
+exports.handler = function (event, context) {
 	const { payload } = JSON.parse(event.body);
+	var post_data = payload.stringify(
+		event.body
+	);
 
-	request.post({
-		headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-		url: 'https://s1010.t.eloqua.com/e/f2',
-		body: payload
-	},
-	function (error, response, body) {
+  // An object of options to indicate where to post to
+  var post_options = {
+      host: 's1010.t.eloqua.com',
+      port: '443',
+      path: '/e/f2',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': Buffer.byteLength(post_data)
+      }
+  };
+
+  // Set up the request
+  var post_req = https.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
 		callback(null, {
-			statusCode: response.statusCode,
-			body:  "Done " + body 
-		});	 
-	});
-};
+			statusCode: 200,
+			body:  "Done" 
+		});	
+      });
+      res.on('error', function (e) {
+		callback(null, {
+			statusCode: 400,
+			body:  "Failed " + e.message 
+		});	 	  
+      });
+
+  });
+
+  // post the data
+  post_req.write(post_data);
+  post_req.end();
+
+}
