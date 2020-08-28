a3cbbd1aecb501838f41406dd77c12e36d484571
Alan Chatfield <47448740+AlanChatfield@users.noreply.github.com>
2020-08-28 21:33:00 +0100

    Create eloqua-form.js

---
+++ functions/eloqua-form.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+exports.handler = async function (event, context, callback) {
+	const { payload } = JSON.parse(event.body)
+
+	request.post({
+		headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+		url: 'https://s1010.t.eloqua.com/e/f2',
+		body: payload;
+	},
+	function (error, response, body) {
+		callback(null, {
+			statusCode: response.statusCode
+			body:  "Done " + body 
+		})	 
+	});
+}
